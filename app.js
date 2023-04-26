75780525c75e037947595fe8a26af1d21174f329
David <davidtonyawusaku@gmail.com>
2023-04-26 15:52:51 +0100

    Version 1

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const https =  require("https");
+const bodyParser = require("body-parser"); 
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs'); // set the view engine to ejs
+
+
+app.get("/", function(req,res){
+    // res.sendFile(__dirname + "//index.html");
+    res.render('index');
+});
+app.post("/", function (req, res){
+    const query= req.body.cityName;
+    const apiKey = "4df172f8d01decd62e7709cc092d3bbd";
+    const url  = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&APPID="+apiKey;
+    https.get(url,function(response){
+        response.on('data',(data)=>{
+            var weatherData = JSON.parse(data);
+            if(weatherData.cod == 404){
+                res.send("Error");
+            }else{
+                var description = weatherData.weather[0].description;
+                var temperature = weatherData.main.temp;
+                var condition = weatherData.weather[0].main  //Yet to use this
+                var icon = weatherData.weather[0].icon;
+                var location = weatherData.name;
+                var imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+                
+                // console.log(weatherData);
+                // res.sendFile(__dirname + "//index.html");
+                res.render('succes',{
+                    description: description,  // property: data aka variable name kini
+                    temperature: temperature,
+                    state: condition,
+                    location: location,
+                })
+                // res.send("<img src= " + imageURL + " alt='weather_icon'><br>Temperature in "+ location+ " is about "+temperature+" degrees Celsuis, and it is "+description );
+            }
+
+        });
+    });
+
+});
+
+// app.post("/weather", function (req) {
+//     const city = req.body.city;
+//  });
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000")
+});
